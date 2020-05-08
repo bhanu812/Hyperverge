8df1cfda2df05e08c529c0d9a2b08aa6b6ab220a
bhanu812 <bhanup812@gmail.com>
2020-05-08 20:58:03 +0530

    status update

--- controllers/TicketController.js
+++ controllers/TicketController.js
@@ -1,70 +1,115 @@
-const express = require("express")
-const Ticket = require("../models/Ticket")
-const User = require("../models/User")
+const express = require("express");
+const Ticket = require("../models/Ticket");
+const User = require("../models/User");
+const { HttpCodes, CustomErrors } = require('../response');
 
 async function update(req, res) {
-  var response={};
-  if(req.body.passenger){
+  var response = {};
+  if (req.body.passenger) {
     let passenger = req.body.passenger;
-    passenger = await User.findOneAndUpdate({id:passenger.id},{$set:passenger},{new:true}); 
+    passenger = await User.findOneAndUpdate({ id: passenger.id }, { $set: passenger }, { new: true });
+    if (!passenger) {
+      return res.status(HttpCodes.OK).send({
+        success: false, Response: [], message: "update is not successfull"
+      })
+    }
     var passenger_id = passenger.id;
-    response.passenger =passenger;
+    response.passenger = passenger;
   }
-  if(req.body.status){
+  if (req.body.status) {
     var passenger_id = req.body.passenger_id;
     var status = req.body.status;
-    if(status === 'open'){
-    var passenger_id = '';
+    if (status === 'open') {
+      var passenger_id = '';
     }
-    var ticket = await Ticket.findOneAndUpdate({ seat_number: req.params.seat },{$set:{"status":status,"passenger":passenger_id} }, { new: true });
+    var ticket = await Ticket.findOneAndUpdate({ seat_number: req.params.seat }, { $set: { "status": status, "passenger": passenger_id } }, { new: true });
+    if (!ticket) {
+      return res.status(HttpCodes.OK).send({
+        success: false, Response: {}, message: "Seat with a given number not exist"
+      })
+    }
     response.ticket = ticket
   }
-  res.send(response);
+
 }
 
 
 
 // get the status of a ticket based on ticket_id
 async function userDetail(req, res) {
-  var ticket = await Ticket.findOne({ seat_number: req.params.seat});
+  var ticket = await Ticket.findOne({ seat_number: req.params.seat });
+  if (!seat_number) {
+    return res.status(HttpCodes.OK).send({
+      success: false, Response: [], message: "Seat with a given number not exist"
+    })
+  }
+
   var user_id = ticket.passenger;
-  if(String(user_id).trim.length == 0)
-  return res.send("no passenger for the ticket");
-  var user = await User.findOne({id:user_id});
-  return res.send(user);
-  
+  if (String(user_id).trim.length == 0)
+    return res.send("no passenger for the ticket");
+  var user = await User.findOne({ id: user_id });
+  return res.status(HttpCodes.OK).send({ success: true, Response: user, message: 'fetch sucessfull' });
+
 }
 
 async function checkStatus(req, res) {
-  var ticket = await Ticket.findOne({ seat_number: req.params.seat},{status:1});
-  res.send(ticket);
-  
+  var ticket = await Ticket.findOne({ seat_number: req.params.seat }, { status: 1 });
+  if (!seat_number) {
+    return res.status(HttpCodes.OK).send({
+      success: false, Response: [], message: "Seat with a given number not exist"
+    })
+  }
+
+
+  res.status(HttpCodes.OK).send({ success: true, Response: ticket, message: 'fetch sucessfull' });
+
 }
 
 async function getDetail(req, res) {
-  var ticket = await Ticket.findOne({ seat_no: req.params.seat});
-  res.send(ticket);
-  
+  var ticket = await Ticket.findOne({ seat_no: req.params.seat });
+  if (!seat_number) {
+    return res.status(HttpCodes.OK).send({
+      success: false, Response: [], message: "Seat with a given number not exist"
+    })
+  }
+
+
+  res.status(HttpCodes.OK).send({ success: true, Response: ticket, message: 'fetch sucessfull' });
+
 }
 // get list of all open tickets
 async function getopenTicket(req, res) {
-  tickets = await Ticket.find({status: "open"});
-  res.send(tickets)
+  tickets = await Ticket.find({ status: "open" });
+  if (!seat_number) {
+    return res.status(HttpCodes.OK).send({
+      success: false, Response: [], message: "Seat with a given number not exist"
+    })
+  }
+
+
+  res.status(HttpCodes.OK).send({ success: true, Response: tickets, message: 'fetch sucessfull' });
 }
 
 // get list of all closed tickets
 async function getcloseTicket(req, res) {
-  tickets = await Ticket.find({status: "closed"});
-  res.send(tickets)
+  tickets = await Ticket.find({ status: "closed" });
+  if (!seat_number) {
+    return res.status(HttpCodes.OK).send({
+      success: false, Response: [], message: "Seat with a given number not exist"
+    })
+  }
+
+
+  res.status(HttpCodes.OK).send({ success: true, Response: tickets, message: 'fetch sucessfull' });
 }
 
 
 
 async function resetTicket(req, res) {
-  var ticket = await Ticket.updateMany({},{$set:{"status":"open","passenger":''}},{ new: true });
-  if(ticket)
-  return res.send('tickets reset')
-  
+  var ticket = await Ticket.updateMany({}, { $set: { "status": "open", "passenger": '' } }, { new: true });
+  if (ticket)
+    return res.status(HttpCodes.OK).send({ success: true, message: "tickets reset" });
+
 }
 
 module.exports = {
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -2,8 +2,11 @@ const express = require("express")
 const router = express.Router()
 const { check, validationResult } = require("express-validator")
 
-const { signup, signin, signout } = require("../controllers/UserController")
-const { userSignupValidator } = require("../validator")
+const { signup, signin } = require("../controllers/UserController")
+
+const {
+    userSignupValidator
+} = require("../validator");
 
 router.post("/signup", userSignupValidator, signup)
 router.post("/signin", signin)
